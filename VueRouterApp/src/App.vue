ccd8342f7f6b182983c5b9c3df744952672a23ff
Ibrahim <nlibrahm@krohne.com>
2022-03-01 15:47:31 +0100

    VueRouterAppCli

--- VueRouterApp/src/App.vue
+++ VueRouterApp/src/App.vue
@@ -9,7 +9,6 @@
   <br>
   <router-link to="/">Home</router-link>
   <router-link to="/about">About</router-link> 
-  
   <router-view></router-view>
 </template>
 
--- vuerouterappvuecli/src/App.vue
+++ vuerouterappvuecli/src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+//import { defineComponent } from "@vue/composition-api";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+//export default defineComponent({
+//  setup() {},
+//});
+</script>
