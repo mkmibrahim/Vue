b47b21260cb5668a674d9e8a763c061b0d561e1b
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2022-03-10 16:23:00 +0100

    20220310

--- vuerouterappvuecli/src/router/index.js
+++ vuerouterappvuecli/src/router/index.js
@@ -12,17 +12,19 @@ const routes = [
     name: "destination.show",
     component: () => import("@/views/DestinationShow.vue"),
     props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experience.show",
+        component: () => import("@/views/ExperienceShow.vue"),
+        props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+      },
+    ],
   },
   {
     path: "/destination2/:id/:slug",
     name: "destination2.show",
     component: () => import("@/views/DestinationShow2.vue"),
-  },
-  {
-    path: "/experience/:id/:experienceSlug",
-    name: "experience.show",
-    component: () => import("@/views/ExperienceShow.vue"),
-    props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
   },
 ];
 
